b0e3eb53459036b39d766ae54ae52051093cff84
drogowska <60966644+drogowska@users.noreply.github.com>
2022-05-22 14:44:00 +0200

    trochę dodane do rozdziału 3

---
+++ Logic/src/main/java/set/Tag.java
@@ -0,0 +1,11 @@
+package set;
+
+import quantifier.MembershipFunction;
+
+import java.util.List;
+
+public class Tag {
+
+    private String label;
+    private List<MembershipFunction> membershipFunctions;
+}
