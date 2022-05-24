46e44be4d0160b7d3b7811d74d2ad3b6cfda7a5b
drogowska <60966644+drogowska@users.noreply.github.com>
2022-05-25 00:12:21 +0200

    update functions cd

--- Logic/src/main/java/functions/TrapezoidalMembershipFunction.java
+++ Logic/src/main/java/functions/TrapezoidalMembershipFunction.java
@@ -1,10 +1,22 @@
 package functions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrapezoidalMembershipFunction extends MembershipFunction {
-    private double a;
-    private double b;
-    private double c;
+    private List<LineFunction> funs = new ArrayList<>();
+
+    public TrapezoidalMembershipFunction(double a1, double b1, double b2, double a3, double b3) {
+        funs.add(new LineFunction(a1, b1));
+        funs.add(new ConstantFunction(b2));
+        funs.add(new LineFunction(a3,b3));
+    }
+
     public Double calculate(double x) {
+        for (LineFunction fun : funs) {
+            if (fun.getUniverseOfDiscourse().contain(x))
+                return fun.calculate(x);
+        }
         return null;
     }
 }
--- Logic/src/main/java/functions/TriangleMembershipFunction.java
+++ Logic/src/main/java/functions/TriangleMembershipFunction.java
@@ -3,10 +3,24 @@ package functions;
 import functions.MembershipFunction;
 
 public class TriangleMembershipFunction extends MembershipFunction {
-    private double a;
-    private double b;
-    private double c;
+
+    private double a1;
+    private double b1;
+    private double a2;
+    private double b2;
+    private LineFunction fun1;
+    private LineFunction fun2;
+
+    public TriangleMembershipFunction(double a1, double b1, double a2, double b2) {
+       fun1 = new LineFunction(a1,b1);
+       fun2 = new LineFunction(a2,b2);
+    }
+
     public Double calculate(double x) {
-        return null;
+        if (fun1.getUniverseOfDiscourse().contain(x))
+            return fun1.calculate(x);
+        else if (fun2.getUniverseOfDiscourse().contain(x))
+            return fun2.calculate(x);
+        return 0.0;
     }
 }
