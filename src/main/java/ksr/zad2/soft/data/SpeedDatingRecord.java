ece844255eccb384a3c682c5c515cb2c85711c36
Mateusz1010 <lolmanek123456@gmail.com>
2022-05-29 14:59:04 +0200

    Fixed entity in java

--- src/main/java/ksr/zad2/soft/data/SpeedDatingRecord.java
+++ src/main/java/ksr/zad2/soft/data/SpeedDatingRecord.java
@@ -37,12 +37,11 @@ public class SpeedDatingRecord {
     private float pref_o_ambitious;
     private float pref_o_shared_interests;
     private String d_pref_o_attractive;
-    private String d_pref_o_sincere;
-    private String d_pref_o_intelligence;
+    private String d_pref_o_sincered_pref_o_intelligence;
     private String d_pref_o_funny;
     private String d_pref_o_ambitious;
     private String d_pref_o_shared_interests;
-    private float attractive_o;
+    private String attractive_o;
     private float sinsere_o;
     private float intelligence_o;
     private float funny_o;
@@ -54,7 +53,7 @@ public class SpeedDatingRecord {
     private String d_funny_o;
     private String d_ambitous_o;
     private String d_shared_interests_o;
-    private float attractive_important;
+    private String attractive_important;
     private float sincere_important;
     private float intellicence_important;
     private float funny_important;
@@ -66,7 +65,7 @@ public class SpeedDatingRecord {
     private String d_funny_important;
     private String d_ambtition_important;
     private String d_shared_interests_important;
-    private float attractive;
+    private String attractive;
     private float sincere;
     private float intelligence;
     private float funny;
@@ -76,7 +75,7 @@ public class SpeedDatingRecord {
     private String d_intelligence;
     private String d_funny;
     private String d_ambition;
-    private float attractive_partner;
+    private String attractive_partner;
     private float sincere_partner;
     private float intelligence_partner;
     private float funny_partner;
@@ -88,7 +87,7 @@ public class SpeedDatingRecord {
     private String d_funny_partner;
     private String d_ambition_partner;
     private String d_shared_interests_partner;
-    private float sports;
+    private String sports;
     private float tvsports;
     private float exercise;
     private float dining;
@@ -105,7 +104,7 @@ public class SpeedDatingRecord {
     private float music;
     private float shopping;
     private float yoga;
-    private String d_sports;
+    private float d_sports;
     private String d_tvsports;
     private String d_exercise;
     private String d_dining;
@@ -122,22 +121,23 @@ public class SpeedDatingRecord {
     private String d_music;
     private String d_shopping;
     private String d_yoga;
-    private float interests_correlate;
-    private String d_interests_correlate;
-    private float expected_happy_with_sd_people;
+    private String interests_correlate;
+    private float d_interests_correlate;
+    private String expected_happy_with_sd_people;
     private float expected_num_interested_in_me;
     private float expected_num_matches;
     private String d_expected_happy_with_sd_people;
     private String d_expected_num_interested_in_me;
     private String d_expected_num_matches;
-    private float _like;
+    private String _like;
     private float guess_prob_liked;
     private String d_like;
     private String d_guess_prob_liked;
-    private int met;
-    private int decision;
-    private int decision_o;
-    private int _match;
+    private String met;
+    private float decision;
+    private float decision_o;
+    private float _match;
+    private float column_123;
 
     public SpeedDatingRecord(int age, int d_age, int importance_same_race, int importance_same_religion, float pref_o_inteligence, float pref_o_ambitious,
                              int tvsports, int expected_num_interested_in_me, float guess_prob_liked, float sincere) {
