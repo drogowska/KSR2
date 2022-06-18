29b921cec59885a22f32256ec4850acd49a994a2
Mateusz1010 <lolmanek123456@gmail.com>
2022-06-18 21:37:17 +0200

    Small change

--- src/main/java/ksr/zad2/soft/gui/SummaryModel.java
+++ src/main/java/ksr/zad2/soft/gui/SummaryModel.java
@@ -148,18 +148,18 @@ public class SummaryModel {
 
     @Override
     public String toString() {
-        return "{summary='" + summary + '\'' +
-                ", T1=" + T1 +
-                ", T2=" + T2 +
-                ", T3=" + T3 +
-                ", T4=" + T4 +
-                ", T5=" + T5 +
-                ", T6=" + T6 +
-                ", T7=" + T7 +
-                ", T8=" + T8 +
-                ", T9=" + T9 +
-                ", T10=" + T10 +
-                ", T11=" + T11 +
-                ", T=" + T + "}";
+        return  summary +
+                " & " + T1 + " " +
+                " & " + T2 + " " +
+                " & " + T3 + " " +
+                " & " + T4 + " " +
+                " & " + T5 + " " +
+                " & " + T6 + " " +
+                " & " + T7 + " " +
+                " & " + T8 + " " +
+                " & " + T9 + " " +
+                " & " + T10 + " " +
+                " & " + T11 + " " +
+                " & " + T;
     }
 }
